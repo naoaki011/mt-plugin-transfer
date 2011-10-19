a96a7251c702fe12b41f7c093c1f9eed146fb7e4
naoaki011 <naoaki@orion.ocn.ne.jp>
2011-10-19 22:03:59 +0900

    fix condition.

--- plugins/Transfer/lib/Transfer/Util.pm
+++ plugins/Transfer/lib/Transfer/Util.pm
@@ -10,19 +10,31 @@ sub is_blog_module {
     return 0 if (!$blog );
     my $blog_id = $blog->id;
     return 1 if $app->param('action_name');
-    my $type = $app->param('filter_key');
-    my $mod_or_wid;
-    if ($type && ($type eq 'module_templates' || $type eq 'widget_templates')) {
-        $mod_or_wid = 1;
+    if ($app->param('filter_key')) {
+        my $type = $app->param('filter_key');
+        if ($blog_id && $type && (($type eq 'module_templates') || ($type eq 'widget_templates'))) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
-    return 1 if ($blog_id && $mod_or_wid);
+    return 1;
 }
 
 sub is_global_module {
     my $app = MT->instance;
     my $blog_id = $app->param('blog_id') || 0;
     return 0 if $blog_id;
-    #FIX condition will work execute code. but has no $app->param('filter_key')!
+    if ($app->param('filter_key')) {
+        my $type = $app->param('filter_key');
+        if ($type && (($type eq 'module_templates') || ($type eq 'widget_templates'))) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
     return 1;
 }
 
@@ -31,21 +43,25 @@ sub is_blog {
     my $blog = $app->blog;
     return 0 if (!$blog );
     return 1
-      if ( $blog->parent_id );
+      if ( $blog->id && $blog->parent_id );
+    return 0;
 }
 
 sub is_index_template {
     my $app = MT->instance;
     my $blog = $app->blog;
     return 0 if (!$blog );
-    my $blog_id = $blog->id;
-    my $type = $app->param('filter_key');
-    if ($type && ($type eq 'index_templates')) {
-        return 1;
+    return 0 unless $blog->id;
+    if ($app->param('filter_key')) {
+        my $type = $app->param('filter_key');
+        if ($type && ($type eq 'index_templates')) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
-    else {
-        return 0;
-    }
+    return 1;
 }
 
 sub is_user_can {
@@ -68,12 +84,14 @@ sub is_mt5 {
     my $version = MT->version_id;
     return 1
       if (($version < 5.1)&&($version >= 5));
+    return 0;
 }
 
 sub is_illiad {
     my $version = MT->version_id;
     return 1
       if ($version >= 5.1);
+    return 0;
 }
 
 sub doLog {
